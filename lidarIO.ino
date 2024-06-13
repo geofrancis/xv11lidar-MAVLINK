24debcf27167a80d7caa18ef1c0ee1950cbcda13
geofrancis <o.o.o.o@hotmail.co.uk>
2024-06-13 17:40:19 +0100

    Update lidarIO.ino

--- lidarIO.ino
+++ lidarIO.ino
@@ -4,14 +4,14 @@ void READLIDAR() {
   if (got_packet) {
     lidarAngle = packet.angle_quad;
     for (int i = 0; i < 3; i++) {
-      int trueangle = lidarAngle * i;
+      int trueangle = (lidarAngle * 4) + i;
       lidardistances[trueangle] = packet.distances[i];
       messageAngle = map(trueangle, 0, 359, 0, 72);
+      distances[messageAngle] = lidardistances[trueangle];
     }
-      if (packet.angle_quad = 89) {
-        newscan = 1;
-          p.neoPixelFill(255, 0, 0, true);
-      }
-    
+    if (packet.angle_quad = 89) {
+      newscan = 1;
+      p.neoPixelFill(255, 0, 0, true);
+    }
   }
 }
