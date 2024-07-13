d8dca9db746a2566efad7ae3816cd9de2458e160
geofrancis <o.o.o.o@hotmail.co.uk>
2024-07-13 18:15:43 +0100

    Update lidarIO.ino

--- lidarIO.ino
+++ lidarIO.ino
@@ -1,14 +1,20 @@
 
 void READLIDAR() {
-  if (packet.angle_quad != lidarAngle) {
-    lidarAngle = packet.angle_quad;
-    if (lidarAngle = 0){newscan = 1;}
-    messageAngle = map(lidarAngle, 0, 89, 0, 72);
-    bool got_packet;
-    got_packet = lidar.processAvailable(&packet);
-    lidar.applyMotorPID();
-    if (got_packet) {
-      distances[messageAngle] = (packet.distances[0] / 10);
+  
+  lidarAngle = packet.angle_quad;
+  messageAngle = map(lidarAngle, 0, 89, 0, 72);
+  bool got_packet;
+  got_packet = lidar.processAvailable(&packet);
+  lidar.applyMotorPID();
+
+  
+  if (got_packet)
+  {
+    distances[messageAngle] = (packet.distances[0] / 10);
+    serialprintdiag();
+}
+    if (packet.angle_quad = 89) {
+      newscan = 1;
+      p.neoPixelFill(255, 0, 0, true);
     }
-  }
 }
