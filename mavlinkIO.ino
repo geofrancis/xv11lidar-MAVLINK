93d2964b3cfc531e67d23bbb7612af0d6060809c
geofrancis <o.o.o.o@hotmail.co.uk>
2024-07-26 17:17:06 +0100

    Update mavlinkIO.ino

--- mavlinkIO.ino
+++ mavlinkIO.ino
@@ -30,7 +30,7 @@ void mavlink_send() {
     p.neoPixelFill(0, 0, 255, true);
     mavlink_msg_obstacle_distance_pack(sysid, compid, &msg, time_usec, sensor_type, distances, increment, min_distance, max_distance, increment_f, angle_offset, frame);
     uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-    Serial.write(buf, len);
+    Serial2.write(buf, len);
 
 
     unsigned long currentMillis = millis();
@@ -40,7 +40,7 @@ void mavlink_send() {
       p.neoPixelFill(0, 255, 0, true);
       mavlink_msg_heartbeat_pack(1, 196, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
       len = mavlink_msg_to_send_buffer(buf, &msg);
-      Serial.write(buf, len);
+      Serial2.write(buf, len);
       newscan = 0;
       send = 0;
       p.neoPixelFill(0, 0, 255, true);
